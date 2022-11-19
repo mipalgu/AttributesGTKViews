fb9f8b38f0e9c15d51ab1d0d7319fac8202b5fb4
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-11-19 19:11:38 +1000

    Added docC dependency.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,14 @@
 
 import PackageDescription
 
+let dependencies: [Package.Dependency]
+
+#if os(Linux)
+dependencies = [.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")]
+#else
+dependencies = []
+#endif
+
 let package = Package(
     name: "AttributesGTKViews",
     products: [
@@ -16,7 +24,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/rhx/gir2swift.git", branch: "main"),
         .package(url: "https://github.com/rhx/SwiftGtk.git",  branch: "gtk4")
-    ],
+    ] + dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
